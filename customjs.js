349d4e9a63042cf4f266ec1aa6fc4ddd0090bf2a
PaymentIntegration <76071372+PaymentIntegration@users.noreply.github.com>
2020-12-16 11:58:48 -0500

    Update customjs.js

--- customjs.js
+++ customjs.js
@@ -2,3 +2,17 @@ alert('TEsting js')
 function checkAlert() {
   alert("kl")
 }
+
+$(document).ready(function(){
+  $('#trnExpYear').blur(function() {
+    var currentYear = (new Date).getFullYear();
+    var expYear = $("#trnExpYear").text());
+    if (expYear === currentYear) {
+       var currentMonth = (new Date).getMonth() + 1;
+       var expMonth = $("#trnExpMonth").text());
+       if (expMonth < currentMonth) {
+         $("#trnExpMonth").css('background-color', 'red')
+       }
+    }
+  })
+});
